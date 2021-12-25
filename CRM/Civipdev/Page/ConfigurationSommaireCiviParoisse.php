f6188bfa2f8ffe58bc8de43d693e10b7ac30bb70
WebmasterBouclier <webmaster@lebouclier.fr>
2021-12-25 19:05:40 +0100

    Finalisation Page Sommaire

--- CRM/Civipdev/Page/ConfigurationSommaireCiviParoisse.php
+++ CRM/Civipdev/Page/ConfigurationSommaireCiviParoisse.php
@@ -1,9 +1,19 @@
 <?php
 use CRM_Civipdev_ExtensionUtil as E;
 
-class CRM_Civipdev_Page_ConfigurationSommaireCiviParoisse extends CRM_Core_Page {
+class CRM_Civipdev_Page_ConfigurationSommaireCiviParoisse  {
 
-  function buildMenuCiviParoisse (&$menu) {
+
+/**
+ * Construction du menu Paroisse
+ * 
+ * @param int     $maxKey                     Rang du menu Paroisse dans les entrées du menu
+ * @param array   $dataMenuPrincipalParoisse  Construction de l'entrée de menu Paroisse
+ * @param string  $dataSubMenuParoisse        Construction du détail du menu, par appel à la fonction 
+ *                                            SubMenuParoisse
+ *
+ */ 
+  static function buildMenuCiviParoisse (&$menu) {
     $maxKey = ( max( array_keys($menu) ) );
 
     $dataMenuPrincipalParoisse = array(
@@ -14,93 +24,11 @@ class CRM_Civipdev_Page_ConfigurationSommaireCiviParoisse extends CRM_Core_Page 
       'separator' => null,
       'active' => 1,
       'icon' => 'crm-i fa-handshake-o',
-      'permission' => null,
+      'permission' => 'access CiviCRM',
     );
 
-    $dataSubMenuParoisse = array(
-      '1' => array(
-        'label' => E::ts('Nouveau Foyer'),
-        'name' => 'menu-formulaire-foyer',
-        'url' => 'civicrm/formulaire-foyer',
-        'navID' => 1,
-        'active' => 1,
-        'separator' => null,
-        'icon' => 'crm-i fa-address-card',
-        'permission' => null,
-        'image' => 'images/form_particulier.png',
-      ),
-      '2' => array(
-        'label' => E::ts('Nouvel Individu'),
-        'name' => 'menu-formulaire-individu',
-        'url' => 'civicrm/controle-individu',
-        'navID' => 2,
-        'active' => 1,
-        'separator' => null,
-        'icon' => 'crm-i fa-user-plus',
-        'permission' => null,
-      ),
-      '3' => array(
-        'label' => E::ts('Nouvelle organisation'),
-        'name' => 'menu-formulaire-organisation',
-        'url' => null,
-        'navID' => 3,
-        'active' => 1,
-        'separator' => 1,
-        'icon' => 'crm-i fa-building-o',
-        'permission' => null,
-      ),
-      '4' => array(
-        'label' => E::ts('Anniversaires'),
-        'name' => 'menu-anniversaires',
-        'url' => '',
-        'navID' => 4,
-        'active' => 1,
-        'separator' => 1,
-        'icon' => 'crm-i fa-birthday-cake',
-        'permission' => null,
-      ),
-      '5' => array(
-        'label' => E::ts('Listes'),
-        'name' => 'menu-listes',
-        'url' => 'civicrm/sommaire-listes',
-        'navID' => 5,
-        'active' => 1,
-        'separator' => null,
-        'icon' => 'crm-i fa-list-alt',
-        'permission' => null,
-      ),
-      '6' => array(
-        'label' => E::ts('Contrôles'),
-        'name' => 'menu-controles',
-        'url' => 'civicrm/controle-qualite',
-        'navID' => 6,
-        'active' => 1,
-        'separator' => 1,
-        'icon' => 'crm-i fa-check-square',
-        'permission' => null,
-      ),
-      '7' => array(
-        'label' => E::ts('Aide en ligne'),
-        'name' => 'menu-aide',
-        'url' => 'civicrm/files/Mode_Emploi_Base_De_Données_Paroissiale_CiviCRM.pdf',
-        'navID' => 6,
-        'active' => 1,
-        'separator' => 1,
-        'icon' => 'crm-i fa-question-circle-o',
-        'permission' => null,
-      ),
-      '8' => array(
-        'label' => E::ts('Paramètres'),
-        'name' => 'menu-parametres',
-        'url' => '',
-        'navID' => 8,
-        'active' => 1,
-        'separator' => 1,
-        'icon' => 'crm-i fa-cog',
-        'permission' => null,
-      ),
-    );
-
+    $dataSubMenuParoisse = self::SubMenuParoisse();
+    
     _Civipdev_civix_insert_navigation_menu($menu, NULL, $dataMenuPrincipalParoisse);
     
     foreach ($dataSubMenuParoisse as $key => $value) {
@@ -108,5 +36,111 @@ class CRM_Civipdev_Page_ConfigurationSommaireCiviParoisse extends CRM_Core_Page 
     }
 
   }
+
+/**
+ * Fonction qui crée chaque ligne du menu Paroisse
+ * 
+ * @param int     $indexMenu  Variable de rang de chaque ligne du menu Paroisse
+ * @param string  $data       Détail de chaque ligne du menu Paroisse
+ * 
+ * @return string             Ensemble des détails (par ligne) du menu Paroisse
+ *  
+ */
+  static function SubMenuParoisse(){
+    $indexMenu = 1;
+
+    $data = [
+        [
+          'label' => E::ts('Nouveau Foyer'),
+          'name' => 'menu-formulaire-foyer',
+          'url' => 'civicrm/formulaire-foyer',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => null,
+          'icon' => 'crm-i fa-address-card',
+          'permission' => 'access CiviCRM',
+          'image' => null,
+        ],
+        [
+          'label' => E::ts('Nouvel Individu'),
+          'name' => 'menu-formulaire-individu',
+          'url' => 'civicrm/formulaire-individu',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => null,
+          'icon' => 'crm-i fa-user-plus',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/form_particulier.png',
+        ],
+        [
+          'label' => E::ts('Nouvelle organisation'),
+          'name' => 'menu-formulaire-organisation',
+          'url' => null,
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => 1,
+          'icon' => 'crm-i fa-building-o',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/form_entreprise.png',
+        ],
+        [
+          'label' => E::ts('Anniversaires'),
+          'name' => 'menu-anniversaires',
+          'url' => '',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => 1,
+          'icon' => 'crm-i fa-birthday-cake',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/anniversaires.png',
+        ],
+        [
+          'label' => E::ts('Listes'),
+          'name' => 'menu-listes',
+          'url' => 'civicrm/sommaire-listes',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => null,
+          'icon' => 'crm-i fa-list-alt',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/listes.png',
+        ],
+        [
+          'label' => E::ts('Contrôles'),
+          'name' => 'menu-controles',
+          'url' => 'civicrm/controle-qualite',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => 1,
+          'icon' => 'crm-i fa-check-square',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/controles.png',
+        ],
+        [
+          'label' => E::ts('Aide en ligne'),
+          'name' => 'menu-aide',
+          'url' => 'civicrm/files/Mode_Emploi_Base_De_Données_Paroissiale_CiviCRM.pdf',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => 1,
+          'icon' => 'crm-i fa-question-circle-o',
+          'permission' => 'access CiviCRM',
+          'image' => 'images/aide.png',
+        ],
+        [
+          'label' => E::ts('Paramètres'),
+          'name' => 'menu-parametres',
+          'url' => '',
+          'navID' => $indexMenu++,
+          'active' => 1,
+          'separator' => 1,
+          'icon' => 'crm-i fa-cog',
+          'permission' => 'access CiviCRM',
+          'image' => null,
+        ],
+      ];
+      return $data;
+  }
+
 
 }
--- templates/CRM/Civipdev/Page/SommaireCiviparoisse.tpl
+++ templates/CRM/Civipdev/Page/SommaireCiviparoisse.tpl
@@ -1,9 +1,9 @@
 {crmStyle ext=civipdev file=css/sommaire-civiparoisse.css}
 
 <div class="page-sommaire-wrapper">
-	{foreach from=$TestDashboard key=k item=row}
+	{foreach from=$PageSommaireCiviParoisse key=k item=row}
 	<div class ="page-sommaire-box">
-		<a href="{crmURL p="civicrm/`$row.URL`"}">
+		<a href="{crmURL p="`$row.URL`"}">
 			<div><img src="{crmResURL ext=civipdev file=$row.Logo}" alt="{$row.Titre}" /></div>
 			<div>{$row.Titre}</div>
 		</a>
